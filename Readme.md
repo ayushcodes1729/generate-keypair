f38ca064be9c21152c48ad110d4b22c3b5764524
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-28 12:46:55 +0000

    Checked Balance of famous wallets on mainnet

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 env
 SECRET_KEY="[list of numbers in secret key]"
 PUBLIC_KEY="your public key here"
+
+## Connection with main-net or devnet cluster
+- Connection(endpoint, commitment='confirmed'): This function connects to the Solana cluster.
+## Get Balance
+- get_balance(pubkey): This function retrieves the balance of a given public key in lamports.
+## Get Pubkey from domain
+- A function called getDomainKeySync(domain) provided by library @bonfida/spl-name-service returns us a public key object from a domain.
+- - Syntax: const pubkey = new PublicKey(getDomainKeySync(domain).pubkey.toBase58());
--- src/check-balance.ts
+++ src/check-balance.ts
@@ -1,9 +1,13 @@
 import "dotenv/config"
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-
-const keyFromEnv = process.env.PUBLIC_KEY || "";
-const pubkey = new PublicKey(keyFromEnv);
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+import { getDomainKeySync} from "@bonfida/spl-name-service";
+// const keyFromEnv = process.env.PUBLIC_KEY;
+// if (!keyFromEnv) {
+//     throw new Error("Couldn't fetch public key");   
+// }
+const domain = "mccann.sol"
+const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+const pubkey = new PublicKey(getDomainKeySync(domain).pubkey.toBase58());
 const balanceInLamports = await connection.getBalance(pubkey);
 const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL;
 console.log(`Finished fetching balance in your wallet at address ${pubkey} and balance is ${balanceInSol}`)
